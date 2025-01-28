26b9464fd5356d61cbfbe6c55357c3c287c67027
ShivaXOR <tudugu108@gmail.com>
2025-01-28 02:46:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 #Flask Backend
+from flask import Flask, request, jsonify
+import re
+
+app = Flask(__name__)
+
+def check_password_strength(password):
+    strength = 0
+    if len(password) >= 8:
+        strength += 1
+    if re.search(r"[A-Z]", password):
+        strength += 1
+    if re.search(r"[a-z]", password):
+        strength += 1
+    if re.search(r"[0-9]", password):
+        strength += 1
+    if re.search(r"[!@#$%^&*()_+{}\[\]:;<>,.?/~`]", password):
+        strength += 1
+    return strength
+
+@app.route("/check", methods=["POST"])
+def check_password():
+    password = request.json.get("password")
+    strength = check_password_strength(password)
+    return jsonify({"strength": strength})
+
+if __name__ == "__main__":
+    app.run(debug=True)
